9de7c5313c919da21acab8b43aac2f0632132a96
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2019-03-15 14:44:38 -0300

    Using SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
 
 # __name__ Python predefined variable, which is set
 #  to the name of the module in which it is used.
 vapp = Flask(__name__)
+vapp.config.from_object(Config)
+db = SQLAlchemy(vapp)  # db is the object that represents the database
+migrate = Migrate(vapp, db)  # db is the object that represents the database
 
 # routes modules is imported at the bottom (NOT at the top)
 # It avoids circular imports, a commom problem with Flask applications
-from app import routes
+from app import routes, models  # isort:skiped
