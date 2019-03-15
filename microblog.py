e786e85306029091c03cf0f978bf595577c6456d
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2019-03-15 14:45:16 -0300

    Flask  shell context

--- microblog.py
+++ microblog.py
@@ -1,1 +1,9 @@
-from app import vapp
+from app import db, vapp
+from app.models import Post, User
+
+
+@vapp.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db, 'User': User, 'Post': Post
+    }
