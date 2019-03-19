7da550ee668a18f827ff44caf4f1e59e8e2c66cf
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2019-03-19 10:31:39 -0300

    Just formating

--- microblog.py
+++ microblog.py
@@ -5,5 +5,7 @@ from app.models import Post, User
 @vapp.shell_context_processor
 def make_shell_context():
     return {
-        'db': db, 'User': User, 'Post': Post
+        'db': db,
+        'User': User,
+        'Post': Post
     }
