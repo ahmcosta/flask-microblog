ea47443688e1793dfe4ff1c7aba3dca4206a10fb
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2019-03-19 13:32:05 -0300

    Including random text generator (Lorem ipsum)

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,6 @@
+import random
+
+import lorem
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
@@ -61,3 +64,24 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+# route has a dynamic component, Flask will accept any 
+# text in that portion of the URL
+@vapp.route('/user/<username>')
+@login_required
+def user(username):
+    # first_or_404() works exactly like first() when there 
+    # are results, but in the case that there are no results 
+    # automatically sends a 404 error back to the client
+    user = User.query.filter_by(username=username).first_or_404()
+    number = random.randint(1, 3)
+    posts = []
+    print('Number: ', number)
+    print('Range: ', range(number))
+    for n in range(number):
+        posts.append({
+            'author': user,
+            'body': lorem.text(),
+        })
+    return render_template('user.html', user=user, posts=posts)
