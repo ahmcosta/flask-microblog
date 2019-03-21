59afc45c1a1b8309f36eadf60a91bbd61720d6e6
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2019-03-21 15:24:37 -0300

    Fix vapp

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import logging
-from logging.handlers import SMTPHandler
+import os
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 from flask import Flask
 from flask_login import LoginManager
@@ -20,15 +21,15 @@ login.login_view = 'login'
 if not vapp.debug:
     if vapp.config['MAIL_SERVER']:
         auth = None
-        if app.config['MAIL_USERNAME'] or vapp.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], vapp.config['MAIL_PASSWORD'])
+        if vapp.config['MAIL_USERNAME'] or vapp.config['MAIL_PASSWORD']:
+            auth = (vapp.config['MAIL_USERNAME'], vapp.config['MAIL_PASSWORD'])
         secure = None
         if vapp.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
             mailhost=(vapp.config['MAIL_SERVER'], vapp.config['MAIL_PORT']),
             fromaddr='no-reply@' + vapp.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            toaddrs=vapp.config['ADMINS'], subject='Microblog Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         vapp.logger.addHandler(mail_handler)
